bd1913b4e5f3b862ec49ee9614d1ba4ea9fcd885
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-23 08:17:46 -0500

    Implemented first-pass solution for singly_number function

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,10 +3,19 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
-    for i in range(len(arr)):
-        # if elem isn't in 
+    newArr = []
 
+    # loop through input array
+    for index, elem in enumerate(arr):
+        # if element hasn't been seen before add to newArr array and remove from input array
+        if elem not in newArr:
+            newArr.append(elem)
+            arr.pop(index)
+
+    # loop through both arrays and find odd element out
+    for elem in newArr:
+        if elem not in arr:
+            return elem
 
 if __name__ == '__main__':
     # Use the main function to test your implementation

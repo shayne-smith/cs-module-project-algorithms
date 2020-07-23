6ff7fa116d4e8cdc53a0d8ff5dd94350019c0fb7
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-23 11:05:06 -0500

    Implemented first-pass solutuion for sliding_window_max problem

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,13 +4,22 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    max_values = []
 
-    pass
+    for i in range(k - 1, len(nums)):
+        largest_number = max(nums[i-k+1 : i+1])
+        max_values.append(largest_number)
+    
+    return max_values
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
+    # k = 3
+
+    # print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+
+    k = 2  
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")

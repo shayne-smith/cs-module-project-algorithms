8e34d98b798e8de0b7764c06b5e372d5ffcb9744
seanmx96 <seanmx96@users.noreply.github.com>
2020-07-23 09:11:05 -0500

    Implemented first-pass solution for moving_zeros problem

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,26 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    
+    # determine number of non-zero values
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            count += 1
 
-    pass
+    # loop through input array
+    i = 0
+    while i < len(arr):
+
+        # if a zero is encountered and index is in non-zero part of array
+        # pop current zero and append to end
+        while arr[i] == 0 and i < count:
+            arr.pop(i)
+            arr.append(0)
+
+        i += 1
+    
+    return arr
 
 
 if __name__ == '__main__':
